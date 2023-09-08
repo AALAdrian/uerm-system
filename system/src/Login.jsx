783d923dc0d62a1a5bc14352fc65c5457c00f07a
james patrick jose <jamespatrickjose.career@gmail.com>
2023-09-08 09:33:31 +0800

    9:33 am

--- system/src/Login.jsx
+++ system/src/Login.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import './Login.css'
 
 function Login() {
+
+  const [eyeIconToggle, setEyeIconToggle] = useState(false);
+
+  function handleLoginSubmit(e){
+    e.preventDefault();
+  }
+
   return (
     <div className='login-container'>
-        <h2>IP MONITOR</h2>
+        <h3>IP MONITOR</h3>
         <div className='login-form-container'>
-            <form>
-                
+            <form onSubmit={handleLoginSubmit}>
+              <h1>Log in</h1>
+              <div className='login-input-container'>
+                <input type='text' placeholder='Username'></input>
+                <i class={`fa ${}`} aria-hidden="true"></i>
+                <input type='password' placeholder='Password'></input>
+                <button>Log in</button>
+              </div>
             </form>
         </div>
     </div>
