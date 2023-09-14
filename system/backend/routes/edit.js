a42675ce49c78af9d03ebd388d2fe0d89ae2a1f5
james patrick jose <jamespatrickjose.career@gmail.com>
2023-09-14 16:28:06 +0800

    4:27pm

--- system/backend/routes/edit.js
+++ system/backend/routes/edit.js
@@ -18,53 +18,66 @@ router.get('/',(req, res) => {
 */
 
 router.patch("/edit/:ipAddress", async (req, res) => {
-    const { ipAddress } = req.params;
-    const updatedData = req.body;
-    console.log("this is the req.body", req.body);
-    console.log("this is the ip address", ipAddress);
-  
-    const sql1 = "SELECT * FROM computer WHERE ip_address = ?";
-    connection.query(sql1, [ipAddress], async (err, result) => {
+  const {
+    ip_address,
+    department,
+    property_code,
+    cpu_model,
+    cpu_serial_no,
+    remarks,
+    hostname,
+  } = req.body;
+  const updatedData = req.body;
+  const { ipAddress } = req.params;
+  console.log("this is the req.body", req.body);
+  console.log("this is the ip address", ipAddress);
+  const sql1 = "SELECT * FROM computer WHERE ip_address = ?";
+  const queryResult = await new Promise((resolve, reject) => {
+    connection.query(sql1, [ipAddress], (err, result) => {
       if (err) {
         console.log(err);
-        res.status(500).json({ error: "An error occurred while fetching data" });
-        return;
+        reject(err);
       }
-  
-      if (result.length === 0) {
-        res.status(404).json({ error: "Record not found" });
-        return;
-      }
-  
-      const existingData = result[0];
-  
-      const setClause = [];
-      const values = [];
-  
-      for (const key in updatedData) {
-        if (updatedData[key] !== existingData[key]) {
-          setClause.push(`${key} = ?`);
-          values.push(updatedData[key]);
-        }
-      }
-  
-      console.log("this is setClause", setClause);
-      console.log("this is the values", values);
-  
-      const sql = `UPDATE computer SET ${setClause.join(", ")} WHERE ip_address = ?`;
-      values.push(ipAddress);
-  
-      connection.query(sql, values, (err, result) => {
-        if (err) {
-          console.log(err);
-          res.status(500).json({ error: "An error occurred while updating data" });
-          return;
-        }
-        console.log("data is edited");
-        res.send("Successfully edited the data");
-      });
+      console.log(result[0]);
+      resolve(result[0]);
     });
   });
-  
+
+  const edited = Object.keys(queryResult);
+  //.filter(key => {
+  //return updatedData[key] != queryResult[key];
+  //})
+  const updated = edited.filter((key) => {
+    return updatedData[key] != queryResult[key];
+  });
+  console.log(edited);
+
+  const setClause = updated.map((column) => {
+    return `${column} = ?`; // Each column should be set to a parameter
+  });
+  console.log("this is setclause", setClause);
+
+  const values = updated.map((column) => {
+    return updatedData[column];
+  });
+
+  console.log("this is the values", values);
+
+  console.log("this is the values", ...values);
+  const sql = `UPDATE computer SET ${setClause.join(
+    ", "
+  )} WHERE ip_address = ?`;
+  connection.query(sql, [...values, ipAddress], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send({
+        err,
+      });
+      return;
+    }
+    console.log("data is edited");
+    res.send("successfully edited the data");
+  });
+});
 
 module.exports = router;
